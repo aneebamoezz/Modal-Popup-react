140951340350ee8b73572c0133b68efad6bee562
aneebamoezz <anibakaleem@gmail.com>
2024-02-28 11:12:11 +0500

    commit initial files

---
+++ src/components/ModalTest.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+
+import "./styles.css";
+
+export default function ModalTest() {
+    
+  const [showModalPopup, setShowModalPopup] = useState(false);
+
+  function handleToggleModalPopup() {
+    setShowModalPopup(!showModalPopup);
+  }
+
+  function onClose() {
+    setShowModalPopup(false);
+  }
+
+  return (
+    <div>
+      <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
+      {showModalPopup && (
+        <Modal
+          id={"custom-id"}
+          header={<h1>Customized Header</h1>}
+          footer={<h1>Custimzed Footer</h1>}
+          onClose={onClose}
+          body={<div>Customized body</div>}
+        />
+      )}
+    </div>
+  );
+}
